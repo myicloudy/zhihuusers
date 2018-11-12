ba3c8f358a65c7b3c545d03ac5e77e5002a21766
maomao <mzf1521@163.com>
2018-11-12 08:24:10 +0800

    zhihuuser0

--- zhihuuser/settings.py
+++ zhihuuser/settings.py
@@ -65,9 +65,10 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'zhihuuser.pipelines.ZhihuuserPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'zhihuuser.pipelines.ZhihuuserPipeline': 300,
+   'zhihuuser.pipelines.MongoPipeline': 400,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
--- zhihuuser/spiders/zhihu.py
+++ zhihuuser/spiders/zhihu.py
@@ -2,7 +2,7 @@
 # import scrapy
 import json
 
-from scrapy import Spider,Request
+from scrapy import Spider, Request
 
 from zhihuuser.items import UserItem
 
@@ -20,27 +20,46 @@ class ZhihuSpider(Spider):
     follows_url = 'https://www.zhihu.com/api/v4/members/{user}/followees?include={include}&offset={offset}&limit={limit}'
     follows_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
 
+    follower_url = "https://www.zhihu.com/api/v4/members/{user}/followers?include={include}&offset={offset}&limit={limit}"
+    follower_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
+
     def start_requests(self):
-        yield Request(self.user_url.format(user=self.start_user),callback=self.parse_user)
-        yield Request(self.follows_url.format(user=self.start_user, include=self.follows_query, offset=0, limit=20),callback=self.parse_follows)
+        yield Request(self.user_url.format(user=self.start_user), callback=self.parse_user)
+        yield Request(self.follows_url.format(user=self.start_user, include=self.follows_query, offset=0, limit=20),
+                      callback=self.parse_follows)
+        yield Request(self.follower_url.format(user=self.start_user, include=self.follower_query, offset=0, limit=20),
+                      callback=self.parse_follower)
 
     def parse_user(self, response):
         result = json.loads(response.text)
         item = UserItem()
         for field in item.fields:
             if field in result.keys():
-                item[field]=result.get(field)
+                item[field] = result.get(field)
         yield item
 
-        yield Request(self.follows_url.format(user=result.get('url_token'), include=self.follows_query, limit=20, offset=0), self.parse_follows)
+        yield Request(
+            self.follows_url.format(user=result.get('url_token'), include=self.follows_query, limit=20, offset=0),
+            self.parse_follows)
 
+        yield Request(
+            self.follower_url.format(user=result.get('url_token'), include=self.follower_query, limit=20, offset=0),
+            self.parse_follower)
 
     def parse_follows(self, response):
         results = json.loads(response.text)
         if 'data' in results.keys():
             for result in results.get('data'):
-                yield Request(self.user_url.format(user=result.get('url_token')),self.parse_user)
-        if 'pading' in results.keys() and results.get('paging').get('is_end') == False:
+                yield Request(self.user_url.format(user=result.get('url_token')), self.parse_user)
+        if 'paging' in results.keys() and results.get('paging').get('is_end') == False:
             next_page = results.get('paging').get('next')
             yield Request(next_page, self.parse_follows)
 
+    def parse_follower(self, response):
+        results = json.loads(response.text)
+        if 'data' in results.keys():
+            for result in results.get('data'):
+                yield Request(self.user_url.format(user=result.get('url_token')), self.parse_user)
+        if 'paging' in results.keys() and results.get('paging').get('is_end') == False:
+            next_page = results.get('paging').get('next')
+            yield Request(next_page, self.parse_follower)
